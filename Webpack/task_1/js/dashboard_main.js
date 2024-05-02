8e91fe6f4b8145b1350e4fe4d955016f044e5f63
Jamie897 <jamiedanielle456@gmail.com>
2024-05-02 12:32:42 -0500

    .

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,17 @@
 // Import jQuery
 import $ from 'jquery';
+// Import debounce function from Lodash
+import debounce from 'lodash/debounce';
+
+// Function to update the counter
+function updateCounter() {
+    let count = 0; // Initialize count
+    return function() {
+        count++; // Increment count each time the button is clicked
+        // Update the content of the paragraph element with id='count'
+        $('#count').text(`${count} clicks on the button`);
+    };
+}
 
 // Wait for the DOM to be ready
 $(document).ready(function() {
@@ -9,4 +21,10 @@ $(document).ready(function() {
     $('body').append('<button id="startBtn">Click here to get started</button>');
     $('body').append('<p id="count"></p>');
     $('body').append('<p>Copyright - Holberton School</p>');
+
+    // Get a reference to the button element
+    const $button = $('#startBtn');
+
+    // Bind the debounce function to the click event of the button
+    $button.on('click', debounce(updateCounter(), 500));
 });
