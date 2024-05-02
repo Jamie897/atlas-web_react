09d89e0e8b0b42d97484ff76687daeb2f74a04af
Jamie897 <jamiedanielle456@gmail.com>
2024-05-02 13:34:00 -0500

    Adding Header

---
+++ Webpack/task_3/modules/header/header.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+console.log('Init header');
+
+// Add logo and H1 title to the header
+$('#logo').append('<div id="logo"></div>');
+$('#header').append('<h1>Holberton Dashboard</h1>');
