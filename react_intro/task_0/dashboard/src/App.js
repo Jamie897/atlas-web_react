07b1d6166571e85fd982dcd82863876a28c98955
Jamie897 <jamiedanielle456@gmail.com>
2024-05-03 12:26:35 -0500

    Adding CSS

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import logo from './atlas_logo.png';
+import logo from './holberton-logo.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="atlas_logo"
-          alt="logo"
-        />
-        <h1>School dashboard</h1>
-      </header>
-
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1>School Dashboard</h1>
+        </header>
       </div>
-
-      <footer className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
-      </footer>
-    </div>
+      <hr></hr>
+      <div>
+        <body className='App-body'>
+          <p className='login'>Login to access the full dashboard</p>
+        </body>
+      </div>
+      <hr></hr>
+      <div>
+        <footer className='App-footer'>
+          <p className='copyright'>Copyright 2024 - holberton School</p>
+        </footer>
+      </div>
+    </>
   );
 }
 
