066a23f71c75ed148c1275905469a4997ae422da
Jamie897 <jamiedanielle456@gmail.com>
2024-05-03 13:14:19 -0500

    .

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -15,6 +15,11 @@ function App() {
       <div>
         <body className='App-body'>
           <p className='login'>Login to access the full dashboard</p>
+          <label className='email' htmlFor="email">Email:</label>
+          <input className='lilSpace' type="email" id="email" name="email"/>
+          <label className='lilSpace' htmlFor="pwd"> Password:</label>
+          <input className='lilSpace' type="password" id="pwd" name="pwd"/>
+          <button className='lilSpace'>OK</button>
         </body>
       </div>
       <hr></hr>
