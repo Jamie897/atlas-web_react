d1c9b79434002d4ebe8d33992d6f63dc539ca0e1
Jamie897 <jamiedanielle456@gmail.com>
2024-05-04 14:10:42 -0500

    Send

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import holbertonLogo from './holberton-logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={holbertonLogo} className="App-logo" alt="Holberton Logo" />
+        <h1>School Dashboard</h1>
       </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2024 - Holberton School</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
