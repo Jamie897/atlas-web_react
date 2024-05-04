e3a173a0086bbedf856cf1467f0916a03a5b298e
Jamie897 <jamiedanielle456@gmail.com>
2024-05-04 14:15:53 -0500

    .

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,22 +1,29 @@
+import logo from './holberton-logo.jpg';
 import './App.css';
-import holbertonLogo from './holberton-logo.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={holbertonLogo} className="App-logo" alt="Holberton Logo" />
-        <h1>School Dashboard</h1>
-      </header>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1>School Dashboard</h1>
+        </header>
       </div>
-      <footer className="App-footer">
-        <p>Copyright 2024 - Holberton School</p>
-      </footer>
-    </div>
+      <hr></hr>
+      <div>
+        <body className='App-body'>
+          <p className='login'>Login to access the full dashboard</p>
+        </body>
+      </div>
+      <hr></hr>
+      <div>
+        <footer className='App-footer'>
+          <p className='copyright'>Copyright 2024 - holberton School</p>
+        </footer>
+      </div>
+    </>
   );
 }
 
 export default App;
-
