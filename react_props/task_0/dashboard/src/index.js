4d97538330db773f3946a584167ea25b57ffd814
Jamie897 <jamiedanielle456@gmail.com>
2024-05-08 19:34:10 -0500

    .

--- react_props/task_0/dashboard/src/index.js
+++ react_props/task_0/dashboard/src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App/App';
-// import Notification  from './Notifications/Notifications'
-// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    {/* <Notification /> */}
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-// import './style.css'
-
-// const div = document.createElement("div");
-// div.innerHTML = "<p>This is a new paragraph.</p>";
-// document.body.appendChild(div);
-
-
-// console.log("Hello, Webpack!");
