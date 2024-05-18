ffbf0b3d109a42cfadbc65402f882cb5a5bda3f4
Jamie897 <jamiedanielle456@gmail.com>
2024-05-18 13:25:37 -0500

    .

--- React_inline_styling/task_1/dashboard/src/App/App.test.js
+++ React_inline_styling/task_1/dashboard/src/App/App.test.js
@@ -1,32 +1,38 @@
-import React from "react";
-import { shallow } from "enzyme";
-import App from "./App";
-import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import { StyleSheetTestUtils } from 'aphrodite';
 
-describe("<App />", () => {
-  it("App renders without any errors", () => {
+describe('<App />', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it('App renders without any errors', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.exists()).toEqual(true);
   });
 
-  it('Verify if CourseList is displayed when isLoggedIn is false', () => {
+  it('Verify if CourseList is not displayed when isLoggedIn is false', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(CourseList)).toHaveLength(0);
+    expect(wrapper.find('CourseList')).toHaveLength(0);
   });
 
-  it('Verify if CourseList is displayed when isLoggedIn is false', () => {
+  it('Verify if CourseList is displayed when isLoggedIn is true', () => {
     const wrapper = shallow(<App isLoggedIn={true} />);
-    expect(wrapper.find(CourseList)).toHaveLength(1);
-    expect(wrapper.find(Login)).toHaveLength(0);
+    expect(wrapper.find('CourseList')).toHaveLength(1);
+    expect(wrapper.find('Login')).toHaveLength(0);
   });
 
-  it('logOut', () => {
+  it('logOut calls logOut prop and shows an alert', () => {
     const logOut = jest.fn(() => undefined);
     const wrapper = shallow(<App logOut={logOut} />);
-    const alert = jest.spyOn(global, 'alert');
-    expect(alert);
-    expect(logOut);
-    jest.restoreAllMocks();
+    wrapper.instance().logOut();
+    expect(logOut).toHaveBeenCalled();
+    expect(global.alert).toHaveBeenCalledWith('Logging you out');
   });
 });
