475cecdcf9fbdbbf93283a1e71aa00fea4229ac2
Jamie897 <jamiedanielle456@gmail.com>
2024-05-20 12:50:21 -0500

    Footer Changes

--- react_state/task_3/dashboard/src/Footer/Footer.js
+++ react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Footer.css';
 import { getFullYear, getFooterCopy } from '../utils/utils';
+import AppContext from '../App/AppContext'; // Import the context
 
 function Footer() {
+  const { user } = useContext(AppContext); // Get the user object from context
+
   return (
     <footer className='footer'>
       <p>
         Copyright {getFullYear()} - {getFooterCopy(true)}
       </p>
+      {user.isLoggedIn && ( // Display the paragraph only when user is logged in
+        <p>
+          <a href="/contact">Contact us</a>
+        </p>
+      )}
     </footer>
   );
 }
