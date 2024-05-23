3839d8bba86431def5f7da0ab46803847657c743
Jamie897 <jamiedanielle456@gmail.com>
2024-05-22 19:53:16 -0500

    I keep modifying the wrong files

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,14 +1,30 @@
+import { schema, normalize } from 'normalizr';
+import * as notificationsData from '../../notifications.json';
 
-import * as notificationsData from "../../notifications.json";
+// Create user entity
+const user = new schema.Entity('users');
 
-// function that accepts userId as an argument and returns all notifications for that user from the notifications.json file.
-// returns list of all notifications for that user from the notifications.json file that matches the given userId
+// Create message entity with the key 'messages' and set idAttribute to 'guid'
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
 
+// Create notification entity with author and context
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+// Export entities and normalization function
+export { user, message, notification };
+
+export const normalizeData = (data) => normalize(data, [notification]);
+
+// Function that accepts userId as an argument and returns all notifications for that user from the notifications.json file
 export const getAllNotificationsByUser = (userId) => {
-  const notifications = notificationsData.default;
+  // Normalize the notifications data
+  const normalizedData = normalizeData(notificationsData.default);
 
-
-  return notifications
-    .filter((notification) => notification.author.id === userId)
-    .map((notification) => notification.context); 
+  // Get notifications for the specified user
+  return Object.values(normalizedData.entities.notifications)
+    .filter((notification) => notification.author === userId)
+    .map((notification) => notification.context);
 };
