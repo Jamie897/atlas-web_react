cb3d3423ce9b8338ffaeacd48094a0d625596f43
Jamie897 <jamiedanielle456@gmail.com>
2024-05-22 19:50:09 -0500

    Testing

--- 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,16 +1,31 @@
-import { normalize } from 'normalizr';
+import { schema, normalize } from 'normalizr';
 import * as notificationsData from '../../notifications.json';
-import { notification } from '../schema/notifications';
+
+// Create user entity
+const user = new schema.Entity('users');
+
+// Create message entity with the key 'messages' and set idAttribute to 'guid'
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+
+// Create notification entity with author and context
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+// Export entities and normalization function
+export { user, message, notification };
+
+export const normalizeData = (data) => normalize(data, [notification]);
 
 // Function that accepts userId as an argument and returns all notifications for that user from the notifications.json file
 export const getAllNotificationsByUser = (userId) => {
   // Normalize the notifications data
-  const normalizedData = normalize(notificationsData.default, [notification]);
+  const normalizedData = normalizeData(notificationsData.default);
 
   // Get notifications for the specified user
-  const userNotifications = Object.values(normalizedData.entities.notifications)
+  return Object.values(normalizedData.entities.notifications)
     .filter((notification) => notification.author === userId)
     .map((notification) => notification.context);
-
-  return userNotifications;
 };
+
