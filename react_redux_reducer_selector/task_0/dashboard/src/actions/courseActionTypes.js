e50d24e836c53f16452d38d369b0553ba3a78e63
Jamie897 <jamiedanielle456@gmail.com>
2024-05-29 17:43:05 -0500

    .

--- react_redux_reducer_selector/task_0/dashboard/src/actions/courseActionTypes.js
+++
@@ -1,7 +0,0 @@
-// src/actions/courseActionTypes.js
-
-// create the action Types
-//  used to define if a user selected or unselected a specific course
-
-export const SELECT_COURSE = "SELECT_COURSE";
-export const UNSELECT_COURSE = "UNSELECT_COURSE";
