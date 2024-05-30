a5a34163ea7ee03f15489bedf12d403fc9387bd7
Jamie897 <jamiedanielle456@gmail.com>
2024-05-30 10:01:38 -0500

    Fixing the reducer

--- react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,33 +1,31 @@
-// notificaitonReducer.js
+import { Map, fromJS } from 'immutable';
 import * as actionTypes from '../actions/notificationActionTypes';
+import { notificationsNormalizer } from '../schema/notifications';
 
-const initialState = {
-    notifications: [],
+
+const initialState = Map({
     filter: 'DEFAULT',
-};
+    notifications: Map(),
+    messages: Map(),
+    users: Map(),
+});
 
 const notificationReducer = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case actionTypes.FETCH_NOTIFICATIONS_SUCCESS:
-            return {
-                ...state,
-                notifications: action.data.map(notification => ({
-                    ...notification,
-                    isRead: false
-                }))
-            };
+            // Use the notificationsNormalizer function
+            const normalizedNotifications = notificationsNormalizer(action.data);
+            return state.mergeDeep({
+                notifications: fromJS(normalizedNotifications.notifications),
+                users: fromJS(normalizedNotifications.users),
+                messages: fromJS(normalizedNotifications.messages),
+            });
+        // When MARK_AS_READ, use the setIn function from Immutable to update the value of the item in the state
         case actionTypes.MARK_AS_READ:
-            return {
-                ...state,
-                notifications: state.notifications.map(notification =>
-                    notification.id === action.index ? { ...notification, isRead: true } : notification
-                )
-            };
+            return state.setIn(['messages', action.guid, 'isRead'], true);
+        // When SET_TYPE_FILTER, use the set function from Immutable to update the value of the state
         case actionTypes.SET_TYPE_FILTER:
-            return {
-                ...state,
-                filter: action.filter
-            };
+            return state.set('filter', action.filter);
         default:
             return state;
     }
