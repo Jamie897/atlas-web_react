af3df0e615fed4f5fb050154e074c700ca35f00e
Jamie897 <jamiedanielle456@gmail.com>
2024-05-30 08:50:55 -0500

    .

--- react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
+++ react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -1,4 +1,5 @@
 // reducers/uiReducer.js
+
 import { Map } from 'immutable';
 import {
     DISPLAY_NOTIFICATION_DRAWER,
@@ -8,39 +9,24 @@ import {
     LOGOUT
 } from '../actions/uiActionTypes';
 
-const initialState = {
+const initialState = Map({
     isNotificationDrawerVisible: false,
     isUserLoggedIn: false,
     user: {}
-};
+});
 
 const uiReducer = (state = initialState, action) => {
     switch (action.type) {
         case DISPLAY_NOTIFICATION_DRAWER:
-            return {
-                ...state,
-                isNotificationDrawerVisible: true
-            };
+            return state.set('isNotificationDrawerVisible', true);
         case HIDE_NOTIFICATION_DRAWER:
-            return {
-                ...state,
-                isNotificationDrawerVisible: false
-            };
+            return state.set('isNotificationDrawerVisible', false);
         case LOGIN_SUCCESS:
-            return {
-                ...state,
-                isUserLoggedIn: true
-            };
+            return state.set('isUserLoggedIn', true);
         case LOGIN_FAILURE:
-            return {
-                ...state,
-                isUserLoggedIn: false
-            };
+            return state.set('isUserLoggedIn', false);
         case LOGOUT:
-            return {
-                ...state,
-                isUserLoggedIn: false
-            };
+            return state.set('isUserLoggedIn', false);
         default:
             return state;
     }
